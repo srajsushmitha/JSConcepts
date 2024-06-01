2bf640e6aa8128beee370c89777db5fd160d1289
Sushmitha <srajsushmitha@gmail.com>
2024-06-02 10:08:38 +1200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 //demonstrating closures along with event listeners
+//! Number will return float whereas parseInt will return whole number
+//? parseInt will take 3 args - element, index and array. 3rd arg is ignored but not 2nd
 function addEventListner(){
   let count = 0;
   document.getElementById('clickme').addEventListener("click", function v(){
